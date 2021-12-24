67e231948bbe615b515e515f9cb0d749e823af42
LTH4823 <77533862+LTH4823@users.noreply.github.com>
2021-12-24 14:23:26 +0900

    check console

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const main = document.querySelector("#main"), 
+
+rack = main.querySelector(".rack"), 
+sissors = main.querySelector(".sissors"), 
+paper = main.querySelector(".paper");
+
+console.log(rack);
+
+function check(){
+}
+
+function resultCal(){
+
+}
+
+function resultImg(){
+    
+}
+
+function resultWord(){
+
+}
+
+function init(){
+    
+}
+
+init();
