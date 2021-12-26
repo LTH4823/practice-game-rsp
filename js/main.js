42d990b35a11fe925deb4b51df4e75d977d7ce87
LTH4823 <77533862+LTH4823@users.noreply.github.com>
2021-12-26 12:43:16 +0900

    change js code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const main = document.querySelector("#main"), 
 rack = main.querySelector(".rack"), 
-sissors = main.querySelector(".sissors"), 
+scissors = main.querySelector(".sissors"), 
 paper = main.querySelector(".paper"),
 user = main.querySelector("#user"),
 opponent = main.querySelector("#opponent"),
@@ -11,12 +11,12 @@ IMG_SIZE_WIDTH = 500,
 IMG_SIZE_HEIGHT = 500;
 
 
-function handCheck(){
+function oppnentHandCheck(){
     const opponentHand = Math.floor(Math.random()*3);
     switch(opponentHand){
         case 0:
             console.log("rack");   
-            opponentImg.src = "../img/rack.png" 
+            //opponentImg.src = "../img/rack.png" 
             break;
         case 1:
             console.log("scissors");    
@@ -32,34 +32,47 @@ function handCheck(){
     }
 }
 
-function resultImg(name){
-    if(name=="rack"){
-        console.log("rack");
-    }else if(name=="scissors"){
-        console.log("scissors");
-    }else if(name=="paper"){
-        console.log("paper");
-    }else{
-        console.log(name);
+function userNumCheck(){
+
+}
+
+function userHandCheck(num){
+    const userHand = num;
+    switch(userHand){
+        case 0:
+            console.log("rack");   
+            //userImg.src = "../img/rack.png" 
+            break;
+        case 1:
+            console.log("scissors");    
+            userImg.src = "../img/scissors.png"
+            break;
+        case 2:
+            console.log("paper");    
+            userImg.src = "../img/paper.png"
+            break;
+        default: 
+            console.log("error");
+            break;
     }
+    
 }
 
 function resultWord(num1, num2){
-    if(num1<num2){
-        console.log("lose");
-    }else if(num1>num2){
-        console.log("win");
-    }else{
-        console.log("error!");
-    }
+    // if(num1<num2){
+    //     console.log("lose");
+    // }else if(num1>num2){
+    //     console.log("win");
+    // }else{
+    //     console.log("error!");
+    // }
 }
 
 function init(){
-    rack.addEventListener("click",resultImg);
-    sissors.addEventListener("click",resultImg);
-    paper.addEventListener("click",resultImg);
-    handCheck();
-    console.log(opponentImg);
+    rack.addEventListener("click",userHandCheck);
+    scissors.addEventListener("click",userHandCheck);
+    paper.addEventListener("click",userHandCheck);
+    oppnentHandCheck();
 }
 
 init();
