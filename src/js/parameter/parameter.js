39d52c2fec54ae1755b83013133c74d52bc2642a
LTH4823 <peter4823zx@naver.com>
2022-01-29 11:47:36 +0900

    seperate css files

--- src/js/parameter/parameter.js
+++ src/js/parameter/parameter.js
@@ -3,9 +3,8 @@ export const user = document.querySelector("#user"),
   userResult = user.querySelector(".user_result"),
   opponent = document.querySelector("#opponent"),
   opponentImg = opponent.querySelector(".opponet_img"),
-  opponentResult = opponent.querySelector(".opponent_result");
-
-export const btns = document.querySelector(".board_btns");
+  opponentResult = opponent.querySelector(".opponent_result"),
+  btns = document.querySelector(".board_btns");
 
 // let hand,
 //   userCount = 0,
